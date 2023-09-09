3a7b5b6aa73ce2d4de805d8068f024be99162e5f
Edison-Mondal <mondaledison@gmail.com>
2023-09-09 22:08:52 +0600

    body parser

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const mongoose = require('mongoose')
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.urlencoded({ extended: false }))
+
+// Use JSON and URL-encoded form data parsing middleware
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 
